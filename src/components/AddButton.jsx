e6370275646c5d3893907bb3e742cb339384c84f
Nona Pareulidze <nona.pareulidze1@gmil.com>
2026-06-25 14:29:29 +0400

    feat: add AddButton component

---
+++ src/components/AddButton.jsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import plus from "../assets/icon-plus.svg";
+export default function AddButton({onClick}) {
+  return (
+    <>
+      <Button onClick={()=>onClick()}>
+        <Div>
+          <Img src={plus} />
+        </Div>
+        New Invoice
+      </Button>
+    </>
+  );
+}
+const Button = styled.button`
+  font-family: Spartan;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 15px;
+  letter-spacing: -0.25px;
+  color: #fff;
+  padding: 8px;
+  border-radius: 24px;
+  width: 150px;
+  height: 48px;
+  border-radius: 24px;
+  display: flex;
+  align-items: center;
+  gap:16px;
+  background-color: #7c5dfa;
+  border: none;
+  &:hover{
+    background-color:#9277FF;
+  }
+`;
+const Div = styled.div`
+  width: 32px;
+  height: 32px;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+`;
+const Img = styled.img`
+  width: 10px;
+  height: 10px;
+  object-fit: cover;
+`;
